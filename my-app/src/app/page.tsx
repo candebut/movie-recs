d02592551b3e36832b0414241a568b1f621111ab
Candela Buttigliero <cbuttigliero@hotmail.com>
2024-07-09 14:45:22 +0200

    styles for modal

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -10,6 +10,11 @@ export default function Home() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isRecommendationModalOpen, setIsRecommendationModalOpen] =
     useState<boolean>(false);
+  const [recommendation, setRecommendation] = useState({
+    name: '',
+    description: '',
+    src: '',
+  });
   const openRecommendationModal = () => {
     setIsRecommendationModalOpen(!isRecommendationModalOpen);
     console.log('Opening the recommendation modal');
@@ -24,14 +29,18 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-evenly p-24'>
       <h2>Te recomiendo esta peli porque</h2>
       {isRecommendationModalOpen ? (
-        <Modal handleClose={() => setIsRecommendationModalOpen(false)}></Modal>
+        <Modal
+          handleClose={() => setIsRecommendationModalOpen(false)}
+          setRecommendation={setRecommendation}
+          recommendation={recommendation}
+        ></Modal>
       ) : null}
       {isLoading ? (
         <PixelLoading />
       ) : (
         <div className='main-container'>
           <PixelButton onClick={() => getFilmRecommendation()}>
-            <p>recibir recomendación</p>
+            <p>recibir recomendacion</p>
           </PixelButton>
           <PixelButton onClick={() => openRecommendationModal()}>
             recomendar
--- my-app/src/assets/components/Modal.tsx
+++ my-app/src/assets/components/Modal.tsx
@@ -1,38 +1,68 @@
 import { IoCloseCircle } from 'react-icons/io5';
-import localFont from 'next/font/local';
 
-const rainbowIt = localFont({ src: '../fonts/Rainbow2000-Italic.ttf' });
-const rainbow = localFont({ src: '../fonts/Rainbow2000-Regular.ttf' });
-const tagHandTrash = localFont({ src: '../fonts/TagHandTrash.otf' });
-const theVintage = localFont({ src: '../fonts/TheVintage.otf' });
-const vintageOne = localFont({ src: '../fonts/VintageOne.ttf' });
-const zai = localFont({ src: '../fonts/zai_HongKongGraffiti.ttf' });
-
-const Modal = (props: { handleClose: () => void }) => {
+const Modal = (props: {
+  handleClose: () => void;
+  setRecommendation: any;
+  recommendation: any;
+}) => {
   return (
-    <div className={`main-modal pixel_modal ${rainbowIt}`}>
-      <div className='modal-close'>
-        <IoCloseCircle onClick={() => props.handleClose()} />
-      </div>
-      <div className='modal-title-wrapper'>
-        <h1>Recomendá una peli que te haya tocado el corazón</h1>
-        <p>
-          Jugatelá, no seas cagón, explicá por qué esa película te tocó el
-          corazón, con quién la viste, por qué te emocionó o por qué es
-          importante para vos. Esto no es una web para cinéfilos snobs, es una
-          web para gente sensible.
-        </p>
-      </div>
-      <div className='modal-form-wrapper'>
-        <label htmlFor='film-name'>Nombre de la película</label>
-        <input name='film-name' id='film_name' />
-        <label htmlFor='film-exp'>Por qué recomendas esta película?</label>
-        <textarea></textarea>
-        <label htmlFor='film-src'>
-          Optativo: dónde verla. Si sabés en qué plataforma encontrarla o tenés
-          algún link gratuito que quieras compartir, dejalo acá
-        </label>
-        <input name='film-name' id='film_name' />
+    <div className={`main-modal pixel_modal`}>
+      <div className='modal-content'>
+        <div className='modal-close'>
+          <IoCloseCircle onClick={() => props.handleClose()} />
+        </div>
+        <div className='modal-title-wrapper'>
+          <h1>Recomendá una peli que te haya tocado el corazón</h1>
+          <p>
+            Jugatelá, no seas cagón, explicá por qué esa película te tocó el
+            corazón, con quién la viste, por qué te emocionó o por qué es
+            importante para vos. Lo que sea que haga que hoy en día te sientas
+            conectado con esa película. Podés ser concreto o explayarte como si
+            fueses uno de esos cinéfilos pretensiosos que escriben 10 párrafos
+            en una review de letterbox. Lo que sí, evitá los spoilers!!
+          </p>
+        </div>
+        <div className='modal-form-wrapper'>
+          <label htmlFor='film-name'>Nombre de la película</label>
+          <input
+            name='film-name'
+            id='film_name'
+            value={props.recommendation.name}
+            onChange={(e) =>
+              props.setRecommendation({
+                ...props.recommendation,
+                name: e.target.value,
+              })
+            }
+          />
+          <label htmlFor='film-exp'>Por qué recomendas esta película?</label>
+          <textarea
+            rows={3}
+            name='film-exp'
+            value={props.recommendation.description}
+            onChange={(e) =>
+              props.setRecommendation({
+                ...props.recommendation,
+                description: e.target.value,
+              })
+            }
+          ></textarea>
+          <label htmlFor='film-src'>
+            Optativo: dónde verla. Si sabés en qué plataforma encontrarla o
+            tenés algún link gratuito que quieras compartir, dejalo acá
+          </label>
+          <input
+            name='film-src'
+            id='film_src'
+            value={props.recommendation.src}
+            onChange={(e) =>
+              props.setRecommendation({
+                ...props.recommendation,
+                src: e.target.value,
+              })
+            }
+          />
+        </div>
       </div>
     </div>
   );
