9347ac06046f90875b37d380f597fdbfc1d87bfb
Candela Buttigliero <cbuttigliero@hotmail.com>
2024-07-09 14:16:03 +0200

    styles for modal

--- my-app/src/assets/components/Modal.tsx
+++ my-app/src/assets/components/Modal.tsx
@@ -1,18 +1,26 @@
 import { IoCloseCircle } from 'react-icons/io5';
+import localFont from 'next/font/local';
+
+const rainbowIt = localFont({ src: '../fonts/Rainbow2000-Italic.ttf' });
+const rainbow = localFont({ src: '../fonts/Rainbow2000-Regular.ttf' });
+const tagHandTrash = localFont({ src: '../fonts/TagHandTrash.otf' });
+const theVintage = localFont({ src: '../fonts/TheVintage.otf' });
+const vintageOne = localFont({ src: '../fonts/VintageOne.ttf' });
+const zai = localFont({ src: '../fonts/zai_HongKongGraffiti.ttf' });
 
 const Modal = (props: { handleClose: () => void }) => {
   return (
-    <div className='main-modal pixel_modal'>
+    <div className={`main-modal pixel_modal ${rainbowIt}`}>
       <div className='modal-close'>
         <IoCloseCircle onClick={() => props.handleClose()} />
       </div>
       <div className='modal-title-wrapper'>
-        <h3>Recomendá una peli que te haya tocado el corazón</h3>
+        <h1>Recomendá una peli que te haya tocado el corazón</h1>
         <p>
           Jugatelá, no seas cagón, explicá por qué esa película te tocó el
           corazón, con quién la viste, por qué te emocionó o por qué es
           importante para vos. Esto no es una web para cinéfilos snobs, es una
-          página para gente sensible
+          web para gente sensible.
         </p>
       </div>
       <div className='modal-form-wrapper'>
